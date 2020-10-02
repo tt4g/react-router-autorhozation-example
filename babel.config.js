679c5e178115d3b8bfc78bb14291a91aacf97ad3
tt4g <mainlinett@gmail.com>
2020-10-02 22:18:40 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -28,6 +28,40 @@ const babelPresetTypescript = [
   },
 ];
 
+const babelPluginTransformRuntime = ["@babel/plugin-transform-runtime"];
+
+const babelPluginTransformRemoveConsole = [
+  "transform-remove-console",
+  { exclude: ["error", "warn"] },
+];
+const babelPluginTransformClassProperties = ["transform-class-properties"];
+
+const babelPluginTransformReactConstantElements = [
+  "@babel/plugin-transform-react-constant-elements",
+];
+
+// Material-UI Minimizing Bundle Size: https://material-ui.com/guides/minimizing-bundle-size/
+const babelPluginImportMaterialUICore = [
+  "babel-plugin-import",
+  {
+    libraryName: "@material-ui/core",
+    // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+    libraryDirectory: "esm",
+    camel2DashComponentName: false,
+  },
+  "core",
+];
+const babelPluginImportMaterialUIIcons = [
+  "babel-plugin-import",
+  {
+    libraryName: "@material-ui/icons",
+    // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+    libraryDirectory: "esm",
+    camel2DashComponentName: false,
+  },
+  "icons",
+];
+
 /**
  * @type {babel.ConfigFunction}
  */
@@ -35,14 +69,38 @@ module.exports = (api) => {
   api.cache.forever();
 
   return {
-    presets: [babelPresetEnv, babelPresetReact, babelPresetTypescript],
     env: {
       production: {
-        plugins: [["transform-remove-console", { exclude: ["error", "warn"] }]],
+        presets: [babelPresetEnv, babelPresetReact, babelPresetTypescript],
+        plugins: [
+          babelPluginTransformRemoveConsole,
+          babelPluginTransformRuntime,
+          babelPluginTransformClassProperties,
+          babelPluginTransformReactConstantElements,
+          babelPluginImportMaterialUICore,
+          babelPluginImportMaterialUIIcons,
+        ],
+      },
+      development: {
+        presets: [babelPresetEnv, babelPresetReact, babelPresetTypescript],
+        plugins: [
+          babelPluginTransformRuntime,
+          babelPluginTransformClassProperties,
+          babelPluginTransformReactConstantElements,
+          babelPluginImportMaterialUICore,
+          babelPluginImportMaterialUIIcons,
+        ],
       },
       // env.test used by jest with babel-jest.
       test: {
         presets: [babelPresetEnvTest, babelPresetReact, babelPresetTypescript],
+        plugins: [
+          babelPluginTransformRuntime,
+          babelPluginTransformClassProperties,
+          babelPluginTransformReactConstantElements,
+          babelPluginImportMaterialUICore,
+          babelPluginImportMaterialUIIcons,
+        ],
       },
     },
   };
